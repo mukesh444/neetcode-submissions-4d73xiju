4e4154b683573c8e6833a114f52b6c6bf1706bcf
mukesh444 <mukeshdutta.246@gmail.com>
2026-04-19 17:10:35 -0400

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        var evaluationStack = [];
+        for (let i=0; i< tokens.length; i++){
+            if (tokens[i] === '+'){
+                var secondElement = evaluationStack.pop();
+                var firstElement = evaluationStack.pop();
+                evaluationStack.push(firstElement + secondElement);
+            }
+            else if (tokens[i] === '/'){
+                var secondElement = evaluationStack.pop();
+                var firstElement = evaluationStack.pop();
+                evaluationStack.push(Math.trunc(firstElement / secondElement));
+            }
+            else if (tokens[i] === '-'){
+                var secondElement = evaluationStack.pop();
+                var firstElement = evaluationStack.pop();
+                evaluationStack.push(firstElement - secondElement);
+            }
+            else if (tokens[i] === '*'){
+                var secondElement = evaluationStack.pop();
+                var firstElement = evaluationStack.pop();
+                evaluationStack.push(firstElement * secondElement);
+            }
+            else{
+                evaluationStack.push(+tokens[i]);
+                console.log(evaluationStack);
+            }
+        }
+
+        return evaluationStack[0];
+    }
+}
