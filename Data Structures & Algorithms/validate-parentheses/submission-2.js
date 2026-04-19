7bd3f2dc5b0a684a1149197f2185d0e83e4dfb24
mukesh444 <mukeshdutta.246@gmail.com>
2026-04-19 16:08:12 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+     var stack = [];
+     for (let i=0; i< s.length; i++){
+        if (s[i] === '[' || s[i] === '{' || s[i] === '('){
+            stack.push(s[i]);
+        }
+        else{
+            if (s[i] === ']' && stack[stack.length - 1] === '['){
+                stack.pop();
+            }
+            else if (s[i] === '}' && stack[stack.length - 1] === '{'){
+                stack.pop();
+            }
+            else if (s[i] === ')' && stack[stack.length - 1] === '('){
+                stack.pop();
+            }
+            else{
+                stack.push(s[i]);
+                break;
+            }
+        }
+     }
+
+     return stack.length === 0;
+    }
+}
