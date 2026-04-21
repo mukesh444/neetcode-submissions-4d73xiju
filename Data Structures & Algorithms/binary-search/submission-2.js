306983c726b768116aea34dde88a591c8683398d
mukesh444 <mukeshdutta.246@gmail.com>
2026-04-20 23:37:17 -0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        var start = 0, end = nums.length - 1, mid = Math.floor((start + end) / 2);
+        var output = -1;
+        
+        while(start <= end){
+            if (nums[mid] > target){
+                end = mid -1;
+                mid = Math.floor((start + end) / 2);
+            }
+            else if (nums[mid] < target){
+                start = mid+1;
+                mid = Math.floor((start + end) / 2);
+            }
+            else{
+                return mid;
+            }
+        }
+
+        return output;
+    }
+}
